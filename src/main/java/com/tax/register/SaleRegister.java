1642feffc674f0ac50f94bbae123b480d2819175
Khurrum Nasim <khurrum.nasim@quadrature.io>
2017-12-07 21:23:45 -0500

    Update SaleRegister.java

--- src/main/java/com/tax/register/SaleRegister.java
+++ src/main/java/com/tax/register/SaleRegister.java
@@ -49,27 +49,4 @@ public class SaleRegister {
              computedSalesTax=0;
          return computedSalesTax + computedImportTax;
     }
-
-    /*public static void main(String ... args) {
-        Product chocolates = new Product("imported box of chocolates", 1000, "food", true);
-        Product perfume = new Product("imported bottle of perfume", 4750, "perfume",true);
-
-        ShoppingCartItem item1 = new ShoppingCartItem(chocolates,1);
-        ShoppingCartItem item2 = new ShoppingCartItem(perfume,1);
-
-        com.tax.common.List list = com.tax.common.List.list(item1,item2);
-
-        ShoppingCart cart = new ShoppingCart(list);
-
-        List<String> taxExemptCategory=new ArrayList<>();
-        taxExemptCategory.add("food");
-        taxExemptCategory.add("books");
-        taxExemptCategory.add("medical");
-
-        SaleRegister register = new SaleRegister(taxExemptCategory);
-
-        Receipt receipt = register.calculateReceipt(cart);
-        System.out.println(receipt.toString());
-        System.out.println("Sales Taxes: " + receipt.getTotalTax() + "\n\nTotal: " + receipt.getTotalCents());
-    }*/
 }
